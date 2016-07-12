c57685e255e238b1326f289f1b4c9ec48af4d3e4
John Musarra <john@mightymu.net>
2016-07-12 15:11:36 -0700

    create app skeleton

--- daphnebot.py
+++ daphnebot.py
@@ -1,2 +1,24 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
 import tweepy
+import time, sys
 print "OK GOOD START TEAM!"
+
+argfile = str(sys.argv[1])
+
+CONSUMER_KEY = 
+CONSUMER_SECRET = 
+ACCESS_KEY =
+ACCESS_SECRET = 
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+filename - open(argfile, 'r')
+f = filename.readlines()
+filename.close()
+
+for line in f:
+    api.update_status(line)
+    time.sleep(900)
