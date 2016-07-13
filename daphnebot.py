b9e4df7cf7ee8115ed8d4bb6187383e6a43c2bfb
John Musarra <john@mightymu.net>
2016-07-12 23:27:58 -0700

    remove auth tokens

--- daphnebot.py
+++ daphnebot.py
@@ -2,23 +2,26 @@
 # -*- coding:utf-8 -*-
 
 import tweepy
-import time, sys
+import time, sys, random
+import auth
 print "OK GOOD START TEAM!"
 
 argfile = str(sys.argv[1])
 
-CONSUMER_KEY = 
-CONSUMER_SECRET = 
-ACCESS_KEY =
-ACCESS_SECRET = 
+CONSUMER_KEY = 'Mg2JY9DS9qSQcEjAIBrR97gFh' 
+CONSUMER_SECRET = auth.CONSUMER_SECRET
+ACCESS_TOKEN = '752964356187357184-WrLmQXh7Vy2ucUNXmH7sZwatOvLhRGa'
+ACCESS_SECRET =  auth.ACCESS_SECRET
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-filename - open(argfile, 'r')
+filename = open(argfile, 'r')
 f = filename.readlines()
 filename.close()
+random.shuffle(f)
 
 for line in f:
     api.update_status(line)
+    print line
     time.sleep(900)
